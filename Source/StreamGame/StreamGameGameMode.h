17d9e0716173dbf28fe063768d479559d1fdf256
Baylee Balsimo Ketelhut <132972988+BayleeGrace@users.noreply.github.com>
2025-09-11 22:23:41 -0700

    TheChamber Environment

---
+++ Source/StreamGame/StreamGameGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "StreamGameGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AStreamGameGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AStreamGameGameMode();
+};
+
+
+
