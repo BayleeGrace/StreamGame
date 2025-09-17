aec78ea6233941f389bf9628d434bd6f0a1eb592
Baylee Balsimo Ketelhut <132972988+BayleeGrace@users.noreply.github.com>
2025-09-17 14:51:01 -0700

    Added TextUtilities Blueprint for Chat functionality

--- Plugins/TextUtilities/Source/TextUtilities/Private/TextUtilitiesBPLibrary.cpp
+++ Plugins/TextUtilities/Source/TextUtilities/Private/TextUtilitiesBPLibrary.cpp
@@ -1,16 +1,28 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "TextUtilitiesBPLibrary.h"
+#include "Engine/DataTable.h"
 #include "TextUtilities.h"
 
-UTextUtilitiesBPLibrary::UTextUtilitiesBPLibrary(const FObjectInitializer& ObjectInitializer)
-: Super(ObjectInitializer)
+
+UTextUtilitiesBPLibrary::UTextUtilitiesBPLibrary()
 {
 
 }
 
-FText GenerateRandomTextFromData(UDataTable* textData) const
+FText UTextUtilitiesBPLibrary::GenerateRandomTextFromData(UDataTable* textData) const
 {
+	// Find a row with the correct tag
+	//FName tagName = FName(tag.ToString());
 	
+
+	// Grab a random (clamped) number in columns 1, 2, and 3
+
+	// Repeat steps 1 and 2 twice [POSSIBLY MAKE THE DESIGNER CHOOSE HOW MANY ITERATIONS]
+
+	// Append each output together to return the FText
+
+	FText textRef = FText::FromString("TestText");
+	return textRef;
 }
 
--- Plugins/TextUtilities/Source/TextUtilities/Public/TextUtilitiesBPLibrary.h
+++ Plugins/TextUtilities/Source/TextUtilities/Public/TextUtilitiesBPLibrary.h
@@ -3,12 +3,16 @@
 #pragma once
 
 #include "Kismet/BlueprintFunctionLibrary.h"
+#include "GameplayTagContainer.h"
 #include "TextUtilitiesBPLibrary.generated.h"
 
 UCLASS()
 class UTextUtilitiesBPLibrary : public UBlueprintFunctionLibrary
 {
-	GENERATED_UCLASS_BODY()
+	GENERATED_BODY()
+
+public:
+	UTextUtilitiesBPLibrary();
 
 	// Function to randomly generate a chat username with a data table
 	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Generate Random Text from Data Table", Keywords = "Text Utilities Generate Text"), Category = "TextUtilities")
